e5c88adbad4e8f3daad02b5596232adfdb58ccfc
Joxxx69 <santiago.gualotuna@hotmail.com>
2025-02-01 14:34:15 -0500

    msg

--- client-gateway/src/main.ts
+++ client-gateway/src/main.ts
@@ -3,9 +3,17 @@ import { AppModule } from './app.module';
 import { envs } from './config/envs.config';
 import { RpcCustomExceptionFilter } from './common/expections/rpc-custom-exception.expection';
 import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted:true 
+    })
+  )
 
   app.useGlobalFilters(new RpcCustomExceptionFilter())
 
