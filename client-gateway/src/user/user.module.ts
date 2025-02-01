56901ede07fc63ca0f075f48ba64dafdf9c3f502
Joxxx69 <santiago.gualotuna@hotmail.com>
2025-02-01 14:31:07 -0500

    cambios

--- client-gateway/src/user/user.module.ts
+++ client-gateway/src/user/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { AuthModule } from 'src/auth/auth.module';
+import { NatsModule } from 'src/transports/nats.module';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService],
+  imports: [NatsModule, AuthModule],
+  providers: [],
 })
 export class UserModule {}
