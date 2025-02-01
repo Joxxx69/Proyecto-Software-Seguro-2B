aa5590498f11f1cae8b65dc3945cd548d54ec50b
Joxxx69 <santiago.gualotuna@hotmail.com>
2025-02-01 14:33:23 -0500

    cambiso

--- client-gateway/src/main.ts
+++ client-gateway/src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config/envs.config';
+import { RpcCustomExceptionFilter } from './common/expections/rpc-custom-exception.expection';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new RpcCustomExceptionFilter())
+
+  app.setGlobalPrefix('api')
+  app.use(cookieParser());
+
   await app.listen(envs.PORT);
 }
 bootstrap();
