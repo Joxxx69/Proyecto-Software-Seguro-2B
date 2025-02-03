47840b9c5bee966d4898d410eff41c12bae7e14d
WSebastianML <wsebastianmoyano@gmail.com>
2025-02-03 18:41:59 -0500

    feat(client-gateway): updated app.module.ts

--- client-gateway/src/app.module.ts
+++ client-gateway/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ConsentModule } from './consent/consent.module';
 
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, ConsentModule],
   controllers: [],
   providers: [],
 })
