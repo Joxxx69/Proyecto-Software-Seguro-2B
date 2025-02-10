c700fc9d0ce8eb5014bc1052d373f8fb1ac0fadc
Joxxx69 <santiago.gualotuna@hotmail.com>
2025-02-10 12:54:16 -0500

    cambios

---
+++ auth-ms/src/auth/enums/audit.enum.ts
@@ -0,0 +1,12 @@
+export enum TipoEvento {
+    ACCESO = 'ACCESO',
+    MODIFICACION = 'MODIFICACION',
+    ELIMINACION = 'ELIMINACION',
+    VIOLACION_SEGURIDAD = 'VIOLACION_SEGURIDAD',
+}
+
+export enum NivelRiesgo {
+    BAJO = 'BAJO',
+    MEDIO = 'MEDIO',
+    ALTO = 'ALTO',
+}
